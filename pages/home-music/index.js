6b2724cd86716f2f0997d0153f65d0668fbfb920
moliang <1790693833@qq.com>
2022-03-09 16:29:02 +0800

    持续更新

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -1,46 +1,139 @@
 // pages/home-music/index.js
-import {getBanners} from '../../service/api_music'
+import {
+  rankingStore
+} from "../../store/index"
+
+import {
+  getBanners,
+  getSongMenu
+} from '../../service/api_music'
 import query_rect from '../../utils/query_rect'
 import throttle from '../../utils/throttle'
 
 const throttleQueryRect = throttle(query_rect)
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-      swiperHeight:'',
-      bannerList:[]
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperHeight: '',
+    bannerList: [],
+    recommendSongMenu: [],
+    recommendSongs: [],
+    playLists: [], // 推荐榜
+    rankings: []
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-      this.getMusicBanner()
-    },
+  },
 
-    // 事件处理
-    headClickInput(){
-        wx.navigateTo({
-          url: '/pages/detail-serrch/index',
-        })
-    },
-
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
     // 获取轮播图数据
-    async getMusicBanner(){
-      let res = await getBanners()
-      this.setData({bannerList:res.banners})
-    },
-    
-    // 获取图片高度
-    handLeSwiper(){
-      throttleQueryRect('.image').then(res=>{
-        const rect = res[0]
-        console.log(rect);
-        this.setData({swiperHeight:rect.height})
+    this.getMusicBanner()
+    getSongMenu().then(res => {
+      this.setData({
+        playLists: res.playlists
+      })
+    })
+
+    //获取热门歌单
+    getSongMenu().then(res => {
+      this.setData({
+        playLists: res.playlists
+      })
+    })
+
+    // 获取推荐歌单
+    getSongMenu('华语').then(res => {
+      this.setData({
+        recommendSongMenu: res.playlists
+      })
+    })
+
+    // 发起共享数据的请求
+    rankingStore.dispatch('getRanKingDataAction')
+
+    //从store获取共享的数据   推荐歌单
+    rankingStore.onState('hotRanking', (res) => {
+      if (!res.tracks) return
+      const recommendSongs = res.tracks.slice(0, 6)
+      this.setData({
+        recommendSongs
+      })
+    })
+    rankingStore.onState("newRanking", this.getRankingHandler(0))  //新歌
+    rankingStore.onState("originRanking", this.getRankingHandler(2)) // 原创
+    rankingStore.onState("upRanking", this.getRankingHandler(3)) // 飙升
+  },
+
+  // 事件处理
+  headClickInput() {
+    wx.navigateTo({
+      url: '/pages/detail-serrch/index',
+    })
+  },
+
+  // 获取轮播图数据
+  async getMusicBanner() {
+    let res = await getBanners()
+    this.setData({
+      bannerList: res.banners
+    })
+  },
+
+  // 获取图片高度
+  handLeSwiper() {
+    throttleQueryRect('.image').then(res => {
+      const rect = res[0]
+      console.log(rect);
+      this.setData({
+        swiperHeight: rect.height
+      })
+    })
+  },
+
+  // onUnload(){
+  //   rankingStore.onState("newRanking",this.getNewRankingHandler)
+  // },
+
+  // 从state获取新歌榜数据
+  // getNewRankingHandler(res) {
+  //   // console.log(res);
+  //   if (Object.keys(res).length === 0) return
+  //   const name = res.name
+  //   const couverImgUrl = res.coverImgUrl
+  //   const songList = res.tracks.slice(0,3)
+  //   const rankingObj = {name,couverImgUrl,songList}
+  //   const originRankings =[... this.data.rankings]
+  //   originRankings.push(rankingObj)
+
+  //   this.setData({ 
+  //     rankings:originRankings
+  //    })
+  // },
+
+  getRankingHandler(idx) {
+    return (res) => {
+      if (Object.keys(res).length === 0) return
+      const name = res.name
+      const couverImgUrl = res.coverImgUrl
+      const playCount = res.playCount
+      const songList = res.tracks.slice(0, 3)
+      const rankingObj = {
+        name,
+        couverImgUrl,
+        songList,
+        playCount
+      }
+      const originRankings = [...this.data.rankings]
+      originRankings.push(rankingObj)
+
+      this.setData({
+        rankings: originRankings
       })
     }
-    
+  },
+
 })
---
+++ store/ranking-store.js
@@ -0,0 +1,50 @@
+import {
+    HYEventStore
+} from "hy-event-store" // 引入库
+
+// 导入API
+import {
+    getRanKing
+} from "../service/api_music"
+
+const rankingMap = {0:"newRanking",1:"hotRanking",2:"originRanking",3:"upRanking"}
+
+const rankingStore = new HYEventStore({
+    state: {
+        newRanking:{}, // 新歌榜
+        hotRanking: {}, //热歌榜
+        originRanking: {}, //原创榜
+        upRanking: {}, //飙升榜
+
+    },
+    actions: {
+        getRanKingDataAction(ctx) {
+            // 0: 新歌榜 1:热歌榜 2：原创榜 3：飙升榜
+            for (let i = 0; i < 4; i++) {
+                getRanKing(i).then(res => {
+                   const rankingName = rankingMap[i]
+                   ctx[rankingName] = res.playlist
+                    // switch (i) {
+                    //     case 0:
+                    //         ctx.newRanking = res.playlist
+                    //         break;
+                    //     case 1:
+                    //         ctx.hotRanking = res.playlist
+                    //         break;
+                    //     case 2:
+                    //         ctx.originRanking = res.playlist
+                    //         break;
+                    //     case 3:
+                    //         ctx.upRanking = res.playlist
+                    //         break;
+                    // }
+                })
+            }
+
+        }
+    }
+})
+
+export {
+    rankingStore
+}
