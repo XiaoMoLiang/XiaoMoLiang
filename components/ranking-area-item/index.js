56db2ee97bb2ca75288a0dbf0e3be4cf9399ce73
moliang <1790693833@qq.com>
2022-03-09 16:52:59 +0800

    首页样式调调整

--- components/ranking-area-item/index.js
+++ components/ranking-area-item/index.js
@@ -1,15 +1,19 @@
 // components/ranking-item/index.js
+import query_rect from "../../utils/query_rect"
 Component({
     /**
      * 组件的属性列表
      */
     properties: {
-        item:{
-            type:Object,
-            value:{}
+        item: {
+            type: Object,
+            value: {},
+            swiperHeight:""
         }
     },
-
+    created() {
+        this.handLeSwiper()
+    },
     /**
      * 组件的初始数据
      */
@@ -21,6 +25,15 @@ Component({
      * 组件的方法列表
      */
     methods: {
-
+        // 获取图片高度
+        handLeSwiper() {
+            query_rect('.right').then(res => {
+                const rect = res[0]
+                console.log(rect,111);
+                this.setData({
+                    swiperHeight: rect.height
+                })
+            })
+        },
     }
 })
